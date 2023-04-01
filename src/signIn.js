58f5133f338563683898dcc4993fa7a741e3db88
Samay Dhawale <samaydhawale1@gmail.com>
2023-04-01 19:26:56 +0530

    doen

--- src/signIn.js
+++ src/signIn.js
@@ -4,3 +4,65 @@ SignUp_btn.addEventListener("click", ()=>{
    location.href = "./signUp.html"
 })
 
+
+let email  = document.querySelector("#email>input")
+let password  = document.querySelector("#password>input")
+let notification =  document.getElementById("notification")
+let button  = document.querySelector("#button")
+
+let userdata = JSON.parse(localStorage.getItem("credentials")) ||[]
+console.log(userdata == "")
+button.addEventListener("click", function(){
+
+   if(userdata == ""){
+      notification.innerText ="Please enter valid information"
+      notification.style.color ="red"
+      notification.style.fontWeight ="bold"
+
+      if(email !== email.value ){
+         email.style.border = "solid red 1px"
+      }
+      if(password !== password.value ){
+         password.style.border = "solid red 1px"
+      }
+   }
+   
+for(let i=0; i<userdata.length; i++){
+
+   if(userdata[i].email == email.value && userdata[i].password == password.value){
+      notification.style.color ="rgb(13, 199, 13)"
+      notification.innerText ="Sign In Successful"
+      notification.style.fontWeight ="bold"
+
+      Swal.fire({
+         position: 'top-end',
+         icon: 'success',
+         title: 'You sign In successfully',
+         showConfirmButton: false,
+         timer: 1500
+       })
+
+       setTimeout(function(){
+         location.href = "./index.html"
+       },1800)
+   }
+   else{
+      notification.innerText ="Please enter valid information"
+      notification.style.color ="red"
+      notification.style.fontWeight ="bold"
+
+      if(userdata[i].email !== email.value ){
+         email.style.border = "solid red 1px"
+      }
+      if(userdata[i].password !== password.value ){
+         password.style.border = "solid red 1px"
+      }
+   }
+}
+})
+
+let logo = document.getElementById("logo");
+
+logo.addEventListener("click", ()=>{
+   location.href = "./index.html"
+})
--- src/signUp.js
+++ src/signUp.js
@@ -3,3 +3,68 @@ let SignUp_btn = document.querySelector("#SignUp_btn>button");
 SignUp_btn.addEventListener("click", ()=>{
    location.href = "./signIn.html"
 })
+
+let name  = document.querySelector("#name>input")
+let phone  = document.querySelector("#phone>input")
+let email  = document.querySelector("#email>input")
+let password  = document.querySelector("#password>input")
+let button  = document.querySelector("#button")
+
+
+
+let userdata = JSON.parse(localStorage.getItem("credentials")) ||[]
+   
+button.addEventListener("click", function(){
+
+   if(name.value ==""){
+      notification.innerText ="All fields are important please enter valid information"
+      notification.style.color ="red"
+      notification.style.fontWeight ="bold"
+
+      if(email.value ==""){
+         email.style.border = "solid red 1px"
+      }
+      if(name.value ==""){
+         name.style.border = "solid red 1px"
+      }
+      if(password.value ==""){
+         password.style.border = "solid red 1px"
+      }
+      if(phone.value ==""){
+         phone.style.border = "solid red 1px"
+      }
+      
+   }
+   else{
+      let obj = {};
+
+      obj.name = name.value
+      obj.phone = phone.value
+      obj.email = email.value
+      obj.password = password.value
+   
+      userdata.push(obj)
+   
+      localStorage.setItem("credentials", JSON.stringify(userdata))
+
+      Swal.fire({
+         position: 'top-end',
+         icon: 'success',
+         title: 'You sign In successfully',
+         showConfirmButton: false,
+         timer: 1500
+       })
+
+       setTimeout(function(){
+         location.href = "./signIn.html"
+       },1500)
+   } 
+})
+
+let logo = document.getElementById("logo");
+
+logo.addEventListener("click", ()=>{
+   location.href = "./index.html"
+})
+
+

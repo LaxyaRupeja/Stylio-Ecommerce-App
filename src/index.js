330b57199bb8af1a8d676d7542e6a337c7ab0c09
pooooh14 <monisha1497@gmail.com>
2023-04-01 20:05:16 +0530

    day_4

--- src/index.js
+++ src/index.js
@@ -19,3 +19,52 @@ for (let i = 0; i < menu_options.length; i++) {
 flycontainer.addEventListener("mouseenter", displayNavContainer);
 flycontainer.addEventListener("mouseleave", hideNavContainer);
 
+
+
+if (localStorage.getItem("credentials") == null) {
+  localStorage.setItem("credentials", JSON.stringify({ username: null }));
+}
+let username = JSON.parse(localStorage.getItem("credentials"));
+let name1 = username.username;
+if (name1 != null) {
+  change_info();
+}
+
+function change_info() {
+  let info = document.getElementById("navbar_info");
+  info.innerHTML = null;
+
+  let div1 = document.createElement("div");
+  let p1 = document.createElement("p");
+  p1.innerText = "Hi " + name1;
+  div1.append(p1);
+
+  let div2 = document.createElement("div");
+  let p2 = document.createElement("p");
+  p2.innerText = "My Account";
+  div2.append(p2);
+
+  let div3 = document.createElement("div");
+  let p3 = document.createElement("p");
+  p3.innerText = "Sign Out";
+  p3.id = "sign_out_btn";
+  div3.append(p3);
+
+  let div4 = document.createElement("div");
+  let p4 = document.createElement("p");
+  p4.innerText = "Customer Care";
+  div4.append(p4);
+
+  info.append(div4, div3, div2, div1);
+}
+
+//signout
+let signout_btn = document.getElementById("sign_out_btn");
+
+function signout() {
+  localStorage.removeItem("credentials");
+  window.location.href = "/";
+}
+if (signout_btn != null) {
+  signout_btn.addEventListener("click", signout);
+}
